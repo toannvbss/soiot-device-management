935fd3c4d9dbbf8d6a793375c86e884c27a2f487
toannv <toannv@bsscommerce.com>
2023-07-03 02:38:51 +0700

    Handle active current page of admin sidebar

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -1,10 +1,16 @@
+'use client';
+
 import React from "react";
+import { usePathname } from 'next/navigation'
 import Link from "next/link";
 import { TbDevicesStar } from "react-icons/tb";
 import { MdDevicesOther } from "react-icons/md";
 import { RxCountdownTimer } from "react-icons/rx";
 import { AiOutlineSetting } from "react-icons/ai";
+
 export function Sidebar() {
+    const pathname = usePathname();
+
     return (
         <div className="fixed h-full w-60 bg-white text-gray-450 border border-r-2 z-50">
             <div className="p-6">
@@ -13,18 +19,18 @@ export function Sidebar() {
                     Device Manager
                 </p>
                 <ul className="mt-6">
-                    <li className="mb-3">
+                    <li className={pathname === "/admin/dashboard" ? "mb-3 text-sky-600 font-bold" : "mb-3"}>
                         <Link href="/admin/dashboard">
                             <MdDevicesOther className="inline-block mr-2" size={20}/>Dashboard
                         </Link>
                     </li>
-                    <li className="mb-3">
-                        <Link href="/admin/dashboard">
+                    <li className={pathname === "/admin/logs" ? "mb-3 text-sky-600 font-bold" : "mb-3"}>
+                        <Link href="/admin/logs">
                             <RxCountdownTimer className="inline-block mr-2" size={20}/>Logs
                         </Link>
                     </li>
-                    <li className="mb-3">
-                        <Link href="/admin/dashboard">
+                    <li className={pathname === "#" ? "mb-3 text-sky-600" : "mb-3"}>
+                        <Link href="#">
                             <AiOutlineSetting className="inline-block mr-2" size={20}/>Setting
                         </Link>
                     </li>
