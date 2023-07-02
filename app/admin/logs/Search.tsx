06091125985bf285f325b76599c7d6d26cbb34e7
toannv <toannv@bsscommerce.com>
2023-07-03 00:16:43 +0700

    Create reset serch term function for logs page

--- app/admin/logs/Search.tsx
+++ app/admin/logs/Search.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { LogContext } from "@/app/Context/LogContext";
 import React, { useContext, useState } from "react";
+import { BsXLg } from "react-icons/bs";
 
 export default function Search() {
     const [searchTerm, setSearchTerm] = useState<string>('');
@@ -12,16 +13,29 @@ export default function Search() {
         }
     }
 
+    const handleResetSearch = () => {
+        setSearchTerm('')
+        if (logContext){
+            logContext.changeSearchTerm('');
+        }
+    }
+
     return (
         <div className="flex items-center justify-end">
             <div className="flex">
-                <input
-                    type="text"
-                    className="block w-full px-4 py-2 text-gray-150 bg-white border rounded-md focus:border-orange-300 focus:ring-orange-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                    placeholder="Search..."
-                    value={searchTerm}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
-                />
+                <div className="relative">
+                    <input
+                        type="text"
+                        className="block w-full pl-4 pr-8 py-2 text-gray-150 bg-white border rounded-md focus:border-orange-300 focus:ring-orange-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        placeholder="Search..."
+                        value={searchTerm}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
+                    />
+                    <BsXLg 
+                        className="absolute cursor-pointer top-1/2 right-3 -translate-y-1/2"
+                        onClick={handleResetSearch}
+                    />
+                </div>
                 <button 
                     className="px-4 ml-2 text-white bg-orange-350 border-l rounded "
                     onClick={handleSearch}
