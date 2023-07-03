4a672da17f8187491b5faff353b88a9a21a55a1d
toannv <toannv@bsscommerce.com>
2023-07-04 03:24:30 +0700

    Add username to header

--- app/admin/Header.tsx
+++ app/admin/Header.tsx
@@ -1,10 +1,21 @@
+'use client'
+
 import { RiAccountCircleFill } from "react-icons/ri";
+import { SessionProvider, useSession } from "next-auth/react";
 
 export function Header() {
+    const { data: session, status } = useSession({
+        required: true,
+    });
+
+    if(status === "loading" && !session) {
+        return <></>
+    }
+    
     return (
         <div className="fixed w-full py-2 bg-white text-gray-600 flex justify-end items-center pr-6 z-10">
             <RiAccountCircleFill className="mr-2" size={25}/>
-            <span className="text-base">Welcome John</span>
+            <span className="text-base">{session && session.user ? `Welcome ${session.user.name}` : "Welcome"}</span>
         </div>
     );
 }
